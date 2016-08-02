d073b5db7aa092a62dd1ed6c86baf7e7a17106ef
30253646@qq.com <dytiger@163.com>
2016-08-02 15:42:28 +0800

    add sql file

---
+++ poem/src/test/resources/poem.sql
@@ -0,0 +1,10 @@
+﻿DROP TABLE IF EXISTS `poem`;
+CREATE TABLE `poem` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) DEFAULT NULL,
+  `author` varchar(50) DEFAULT NULL,
+  `poem_text` varchar(5000) DEFAULT NULL,
+  `dynasty` varchar(50) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
